4c8e28c30c8a2eb71341f5ca4f9a9b9fec499b0d
yuning <ning.dreamer@gmail.com>
2020-07-09 22:28:43 +0800

    将前端主页访问后台获取所有IteamCat配置redis缓存

--- taotao-manage/taotao-manage-service/src/main/java/com/taotao/manage/service/ItemCatService.java
+++ taotao-manage/taotao-manage-service/src/main/java/com/taotao/manage/service/ItemCatService.java
@@ -1,10 +1,14 @@
 package com.taotao.manage.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.abel533.entity.Example;
 import com.taotao.common.bean.ItemCatData;
 import com.taotao.common.bean.ItemCatData2;
 import com.taotao.common.bean.ItemCatResult;
 import com.taotao.manage.mapper.ItemCatMapper;
 import com.taotao.manage.pojo.ItemCat;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,18 +23,38 @@ import java.util.Map;
  */
 
 @Service
-public class ItemCatService extends BaseService<ItemCat>{
+public class ItemCatService extends BaseService<ItemCat> {
 
     @Autowired
     private ItemCatMapper itemCatMapper;
 
+    @Autowired
+    private RedisService redisService;
+
+    @Autowired
+    private PropertiesService propertiesService;
+    //jackson的ObjectMapper
+    private static final ObjectMapper MAPPER = new ObjectMapper();
 
     public ItemCatResult queryItemCatAllToTree() {
+        //获取key
+        String redisKey = propertiesService.REDIS_ITEM_CAT_ALL_KEY;
+        try {
+            //查询缓存
+            String jsonResult = redisService.get(redisKey);
+            //判断是否命中
+            if (StringUtils.isNotEmpty(jsonResult)) {
+                return MAPPER.readValue(jsonResult,ItemCatResult.class);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        //缓存未命中查询数据库
         ItemCatResult itemCatResult = new ItemCatResult();
         List<ItemCat> itemCats = super.queryAll();
         Map<Long, List<ItemCat>> map = new HashMap<Long, List<ItemCat>>();
-        for (ItemCat itemCat:itemCats) {
-            if (!map.containsKey(itemCat.getParentId())){
+        for (ItemCat itemCat : itemCats) {
+            if (!map.containsKey(itemCat.getParentId())) {
                 map.put(itemCat.getParentId(), new ArrayList<ItemCat>());
             }
             map.get(itemCat.getParentId()).add(itemCat);
@@ -38,28 +62,35 @@ public class ItemCatService extends BaseService<ItemCat>{
         ArrayList<ItemCatData> itemCatDatasl = new ArrayList<>();
         itemCatResult.setItemCats(itemCatDatasl);
         List<ItemCat> itemCats1 = map.get(0L);
-        for (ItemCat itemCat1:itemCats1) {
+        for (ItemCat itemCat1 : itemCats1) {
             ItemCatData itemCatData = new ItemCatData();
             itemCatDatasl.add(itemCatData);
-            itemCatData.setUrl("/products"+itemCat1.getName()+".html");
-            itemCatData.setName("<a href='"+itemCatData.getUrl()+"'>"+itemCat1.getName()+"<a>");
+            itemCatData.setUrl("/products" + itemCat1.getName() + ".html");
+            itemCatData.setName("<a href='" + itemCatData.getUrl() + "'>" + itemCat1.getName() + "<a>");
             ArrayList<ItemCatData2> itemCatData2s = new ArrayList<>();
             itemCatData.setItemCatDatas(itemCatData2s);
             List<ItemCat> itemCats2 = map.get(itemCat1.getId());
-            for (ItemCat itemCat2:itemCats2) {
+            for (ItemCat itemCat2 : itemCats2) {
                 ItemCatData2 itemCatData2 = new ItemCatData2();
                 itemCatData2s.add(itemCatData2);
-                itemCatData2.setUrl("/products"+itemCat2.getName()+".html");
-                itemCatData2.setName("<a href='"+itemCatData2.getUrl()+"'>"+itemCat2.getName()+"<a>");
+                itemCatData2.setUrl("/products" + itemCat2.getName() + ".html");
+                itemCatData2.setName("<a href='" + itemCatData2.getUrl() + "'>" + itemCat2.getName() + "<a>");
                 ArrayList<String> strings = new ArrayList<>();
                 itemCatData2.setItemCatStr(strings);
                 List<ItemCat> itemCats3 = map.get(itemCat2.getId());
-                for (ItemCat itemCat3:itemCats3) {
-                    strings.add("/products/" +itemCat3.getId()+ ".html|" + itemCat3.getName());
+                for (ItemCat itemCat3 : itemCats3) {
+                    strings.add("/products/" + itemCat3.getId() + ".html|" + itemCat3.getName());
                 }
             }
 
         }
+        //查询数据库后,放入redis缓存
+        try {
+            redisService.set(redisKey,MAPPER.writeValueAsString(itemCatResult),propertiesService.REDIS_ITEM_CAT_AKK_TIME);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        //通过数据库返回数据
         return itemCatResult;
     }
 
--- taotao-manage/taotao-manage-service/src/main/java/com/taotao/manage/service/PropertiesService.java
+++ taotao-manage/taotao-manage-service/src/main/java/com/taotao/manage/service/PropertiesService.java
@@ -5,12 +5,21 @@ import org.springframework.stereotype.Service;
 
 /**
  * Created by ning_ on 2020/6/21.
- *
  */
 @Service
 public class PropertiesService {
+    //图片上传到的 实际服务器路径
     @Value("${REPOSITORY_PATH}")
     public String REPOSITORY_PATH;
+    //客户端访问图片的域名配置
     @Value("${IMAGE_BASE_URL}")
     public String IMAGE_BASE_URL;
+
+    //redis缓存的查询所有内容分类Itemcat的key
+    @Value("${REDIS_ITEM_CAT_ALL_KEY}")
+    public String REDIS_ITEM_CAT_ALL_KEY;
+
+    //redis缓存中内容分类Itemcats的生存时间
+    @Value("${REDIS_ITEM_CAT_AKK_TIME}")
+    public Integer REDIS_ITEM_CAT_AKK_TIME;
 }
